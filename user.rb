a718da5408e37473d96e9e358ee2518f9166f8a1
Timmy_MT <mathcore_detected@mail.ru>
2019-06-08 20:16:14 +0400

    8 june 16:15

--- user.rb
+++ user.rb
@@ -10,15 +10,17 @@ class User
   def score
     @points = 0
     have_a = false
-    @cards.each do |c|
-      check_card = c.card.chop
-      if (check_card ==  'J') || (check_card ==  'Q') || (check_card ==  'K')
+    @cards.each do |card|
+      # check_card = c.card.chop
+      if %w[J Q K].include?(card)
+      # if (check_card ==  'J') || (check_card ==  'Q') || (check_card ==  'K')
         @points += 10
-      elsif check_card == "A"
+      elsif %w[A].include?(card)
+      # elsif check_card == "A"
         @points += 11
         have_a = true
       else
-        @points += check_card.to_i
+        @points += card.card.chop.to_i #check_card.to_i
       end
     end
     @points -= 10 if @points > 21 && have_a == true
